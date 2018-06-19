974698f2dcf72626d46182a82d16d6a53fb35502
Ianc <ianksamacs>
2018-06-19 10:02:29 -0300

    Up desenhador

---
+++ desenhar.py
@@ -0,0 +1,75 @@
+from tkinter import *
+import tkinter.font
+
+
+class PaintApp:
+    # Stores current drawing tool used
+    drawing_tool = "pencil"
+
+    # Tracks whether left mouse is down
+    left_but = "up"
+
+    # x and y positions for drawing with pencil
+    x_pos, y_pos = None, None
+
+    # Tracks x & y when the mouse is clicked and released
+    x1_line_pt, y1_line_pt, x2_line_pt, y2_line_pt = None, None, None, None
+
+    # ---------- CATCH MOUSE UP ----------
+
+    def left_but_down(self, event=None):
+        self.left_but = "down"
+
+        # Set x & y when mouse is clicked
+        self.x1_line_pt = event.x
+        self.y1_line_pt = event.y
+
+    # ---------- CATCH MOUSE UP ----------
+
+    def left_but_up(self, event=None):
+        self.left_but = "up"
+
+        # Reset the line
+        self.x_pos = None
+        self.y_pos = None
+
+        # Set x & y when mouse is released
+        self.x2_line_pt = event.x
+        self.y2_line_pt = event.y
+
+
+
+    # ---------- CATCH MOUSE MOVEMENT ----------
+
+    def motion(self, event=None):
+
+        if self.drawing_tool == "pencil":
+            self.pencil_draw(event)
+
+    # ---------- DRAW PENCIL ----------
+
+    def pencil_draw(self, event=None):
+        if self.left_but == "down":
+
+            # Make sure x and y have a value
+            if self.x_pos is not None and self.y_pos is not None:
+                event.widget.create_line(self.x_pos, self.y_pos, event.x, event.y, smooth=TRUE, width=5)
+
+            self.x_pos = event.x
+            self.y_pos = event.y
+
+
+
+    def __init__(self, root):
+        drawing_area = Canvas(root)
+        drawing_area.pack()
+        drawing_area.bind("<Motion>", self.motion)
+        drawing_area.bind("<ButtonPress-1>", self.left_but_down)
+        drawing_area.bind("<ButtonRelease-1>", self.left_but_up)
+
+
+root = Tk()
+
+paint_app = PaintApp(root)
+
+root.mainloop()
