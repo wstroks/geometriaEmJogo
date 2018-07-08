1a2f9fe5f5f2450f8722844a4b30523008f95bb2
Ianc <ianksamacs>
2018-07-08 02:56:09 -0300

    Meta do dia OK
    Exibe o resultado, solicita uma forma geometrica especifica, e limpa tela.

--- desenhar.py
+++ desenhar.py
@@ -9,16 +9,21 @@ from PIL import ImageTk, Image, ImageDraw
 import PIL
 from tkinter import *
 import os, sys
+import win32api
+from random import randrange
 
 import tensorflow as tf
 
 
 
-width = 500
-height = 500
-center = height//2
+game_name = "The Best"
+canvas_width = 500
+canvas_height = 500
+center = canvas_height//2
 white = (255, 255, 255)
 green = (0,128,0)
+forma = ""
+    
 
 def tensor(nome):
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -53,31 +58,52 @@ def tensor(nome):
             human_string = label_lines[node_id]
             score = predictions[0][node_id]
             print('%s (score = %.5f)' % (human_string, score))
+        if(label_lines[0] == forma):
+            win32api.MessageBox(0, "Parabens ", game_name, 0x00001000)
+        else:
+            win32api.MessageBox(0, "Tente Novamente", game_name, 0x00001000) 
+        clean()
             
- 
+def start():
+    opcao = randrange(1, 3)
+    if(opcao == 1):
+        forma = "Quadrado"
+    elif(opcao == 2):
+        forma  = "Triangulo"
+    else :
+        forma = "Circulo"
+    win32api.MessageBox(0, "Desenhe um %s " % (forma), game_name, 0x00001000) 
+    
 def save():
     filename = "image.jpg"
     image1.save(filename)
     tensor(filename)
    
-    
+def clean():
+    #print("testando")
+    cv.create_rectangle(0,0, canvas_width+10, canvas_height+10, fill="white")
+    start()
 
+    
 def paint(event):
     # python_green = "#476042"
     x1, y1 = (event.x - 1), (event.y - 1)
     x2, y2 = (event.x + 1), (event.y + 1)
     cv.create_oval(x1, y1, x2, y2, fill="black",width=5)
-    draw.line([x1, y1, x2, y2],fill="black",width=5)
+    #draw.line([x1, y1, x2, y2],fill="black",width=5)
 
 root = Tk()
 
 # Tkinter create a canvas to draw on
-cv = Canvas(root, width=width, height=height, bg='white')
+cv = Canvas(root, width=canvas_width, height=canvas_height, bg='white')
+root.title(game_name)
 cv.pack()
+start()
+
 
 # PIL create an empty image and draw object to draw on
 # memory only, not visible
-image1 = PIL.Image.new("RGB", (width, height), white)
+image1 = PIL.Image.new("RGB", (canvas_width, canvas_height), white)
 draw = ImageDraw.Draw(image1)
 
 # do the Tkinter canvas drawings (visible)
@@ -93,5 +119,8 @@ cv.bind("<B1-Motion>", paint)
 #filename = "my_drawing.png"
 #image1.save(filename)
 button=Button(text="save",command=save)
+button2=Button(text="clean",command=clean)
 button.pack()
+button2.pack()
 root.mainloop()
+
